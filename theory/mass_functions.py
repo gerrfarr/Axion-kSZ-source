ff1476e0cddc62ed6a82d33ede2aab05da1c7e57
Gerrit Farren <gerrit.farre@gmail.com>
2021-01-27 18:21:53 +0000

    added a comment specifying the units of the mass function given

--- theory/mass_functions.py
+++ theory/mass_functions.py
@@ -15,6 +15,7 @@ class MassFunction(object):
 
 class PressSchechterMassFunction(MassFunction):
     def __call__(self, m, z):
+        #output is dN/d log m
         vals = np.sqrt(2 / np.pi) * (self.cosmo.rho_mean * self.cosmo.delta_crit / self.sigmaInt(m, z) / m) * np.fabs(self.sigmaInt.dlogSigma_dlogm(m, z)) * np.exp(-self.cosmo.delta_crit**2 / (2 * self.sigmaInt(m, z)**2))
         return vals
 
