a164885167b3cd4bdaf9af8e4e7a27cac6c17719
Gerrit Farren <gerrit.farre@gmail.com>
2021-01-27 18:22:21 +0000

    implemented fast approximation scheme

--- theory/halo_bias_new.py
+++ theory/halo_bias_new.py
@@ -8,6 +8,7 @@ from scipy.interpolate import interp2d, RectBivariateSpline
 import warnings
 import mcfit
 from scipy.integrate import quad as spIntegrate
+from scipy.integrate import cumtrapz as spCumIntegrate
 
 
 class HaloBias(HaloBiasBase):
@@ -26,7 +27,13 @@ class HaloBias(HaloBiasBase):
         self.__interpolator_B = None
         self.__interpolator_N = None
 
-        self._nbar = [spIntegrate(lambda logM: mass_function(np.exp(logM), z), np.log(mMin), np.log(mMax))[0] for z in self._z_vals]
+        self._nbar = np.array([spIntegrate(lambda logM: mass_function(np.exp(logM), z), np.log(mMin), np.log(mMax))[0] for z in self._z_vals])
+
+        self._b_mean = np.array([spIntegrate(lambda logM: self.simple_bias(np.exp(logM), z) * self._mass_function(np.exp(logM), z), np.log(self._mMin), np.log(self._mMax))[0] for z in self._z_vals]) / self._nbar
+        self._m_mean = np.array([spIntegrate(lambda logM: np.exp(logM) * self._mass_function(np.exp(logM), z), np.log(self._mMin), np.log(self._mMax))[0] for z in self._z_vals]) / self._nbar
+
+        self._b_assign_func = lambda z: self._b_mean[np.where(z == self._z_vals[:, None])[0]].reshape(np.array(z).shape)
+        self._m_assign_func = lambda z: self._m_mean[np.where(z == self._z_vals[:, None])[0]].reshape(np.array(z).shape)
 
     def mass_averaged_bias(self, k_vals, z_vals, mMin, mMax):
         rmin, rmax = self.radius_of_mass(mMin), self.radius_of_mass(mMax)
@@ -63,17 +70,51 @@ class HaloBias(HaloBiasBase):
         self.__interpolator_B = RectBivariateSpline(np.log10(self._k_vals), self._z_vals, self.__bias_vals.T, bbox=[np.min(np.log10(self._k_vals)), np.max(np.log10(self._k_vals)), np.min(self._z_vals), np.max(self._z_vals)], kx=3, ky=1, s=0)
         self.__interpolator_N = RectBivariateSpline(np.log10(self._k_vals), self._z_vals, self.__n_vals.T, bbox=[np.min(np.log10(self._k_vals)), np.max(np.log10(self._k_vals)), np.min(self._z_vals), np.max(self._z_vals)], kx=3, ky=1, s=0)
 
-    def __call__(self, k, z, q):
+    def compute_approximation(self, N=50000):
+
+        if self._window_function=="top_hat" or self._window_function=="gaussian":
+            class interp_emulator:
+                def __init__(self, func):
+                    self.__func = func
+
+                def ev(self, *args):
+                    return self.__func(*args)
+
+            self.__interpolator_B = interp_emulator(lambda logk, z: self._b_assign_func(z)*self.window(10**logk*self.radius_of_mass(self._m_assign_func(z))))
+            self.__interpolator_N = interp_emulator(lambda logk, z: self.window(10**logk * self.radius_of_mass(self._m_assign_func(z))))
+
+        elif self._window_function=="sharp_k":
+            ms = np.logspace(np.log10(self._mMin), np.log10(self._mMax), N)
+            mMesh,zMesh = np.meshgrid(ms, self._z_vals)
+            dump,nBarMesh = np.meshgrid(ms, self._nbar)
+
+            self.__bias_vals = np.flip(spCumIntegrate(self.simple_bias(mMesh, zMesh) * self._mass_function(mMesh, zMesh), np.log(ms), initial=0.0) / nBarMesh, axis=-1)
+            self.__n_vals = np.flip(spCumIntegrate(self._mass_function(mMesh, zMesh), np.log(ms), initial=0.0) / nBarMesh, axis=-1)
+
+            self._k_vals = np.flip(1/self.radius_of_mass(ms))
+            self.__interpolator_B = RectBivariateSpline(np.log10(self._k_vals), self._z_vals, self.__bias_vals.T, bbox=[np.min(np.log10(self._k_vals)), np.max(np.log10(self._k_vals)), np.min(self._z_vals), np.max(self._z_vals)], kx=3, ky=1, s=0)
+            self.__interpolator_N = RectBivariateSpline(np.log10(self._k_vals), self._z_vals, self.__n_vals.T, bbox=[np.min(np.log10(self._k_vals)), np.max(np.log10(self._k_vals)), np.min(self._z_vals), np.max(self._z_vals)], kx=3, ky=1, s=0)
+        else:
+            raise Exception("Unknown Window function: "+str(self._window_function))
+
+    def __call__(self, k, z, q, approximate=False):
         if q == 0:
             vals = np.squeeze(self.__interpolator_N.ev(np.log10(k), z))
+            vals[k > np.max(self._k_vals)] = 0.0
+            vals[k < np.min(self._k_vals)] = 1.0
         elif q == 1:
             vals = np.squeeze(self.__interpolator_B.ev(np.log10(k), z))
+            vals[k > np.max(self._k_vals)] = 0.0
+            try:
+                vals[k < np.min(self._k_vals)] = self._b_assign_func(z[k < np.min(self._k_vals)])
+            except (IndexError, TypeError):
+                vals[k < np.min(self._k_vals)] = self._b_assign_func(z)
         else:
             raise Exception("q={} is not defined.".format(q))
 
-        vals[k > np.max(self._k_vals)] = 0.0
-        vals[k < np.min(self._k_vals)] = 0.0
         return vals
+
+
 
 class HaloBiasFFTLog(HaloBias):
     def __init__(self, cosmo, sigma, mass_function, mMin, mMax, kMin, kMax, z_vals, Nk=1024, window_function='top_hat'):
