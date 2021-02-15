1eef192e318af98bfb9cabcda192c1a32fb0b155
Gerrit Farren <gerrit.farre@gmail.com>
2021-02-15 10:17:58 +0000

    generate cosmo_identifier added

--- auxiliary/helper_functions.py
+++ auxiliary/helper_functions.py
@@ -1,4 +1,4 @@
-
+from ..theory.cosmology import Cosmology, CosmologyCustomH
 
 def is_array(item, cl=None):
     try:
@@ -10,3 +10,10 @@ def is_array(item, cl=None):
         return False
     except AttributeError as ex:
         return False
+
+def generate_cosmo_identifier(cosmo):
+    """
+
+    :type cosmo: Cosmology
+    """
+    return hash((cosmo.h, cosmo.omegaCDM, cosmo.omegaB, cosmo.omega_axion, cosmo.m_axion, cosmo.n_s, cosmo.A_s, type(cosmo) == CosmologyCustomH))
