851bc21bb4a4bc66b6909741477884249b427cc9
Gerrit Farren <gerrit.farre@gmail.com>
2021-02-23 14:25:15 +0000

    added function to get derivative sets

--- fisher_analysis/compute_fisher_matrix.py
+++ fisher_analysis/compute_fisher_matrix.py
@@ -1,4 +1,6 @@
 import numpy as np
+from copy import copy
+from axion_kSZ_source.auxiliary.helper_functions import is_array
 
 
 def make_fisher_matrix(cosmo_derivs, nuisance_derivs, z_vals, covmats):
@@ -31,3 +33,35 @@ def make_fisher_matrix(cosmo_derivs, nuisance_derivs, z_vals, covmats):
                     fisher_matrix[pos_j,pos_i]=fisher_matrix[pos_i,pos_j]
 
     return fisher_matrix
+
+
+def get_deriv_sets(all_derivs, params, param_step_sizes):
+    if params[0] in param_step_sizes.keys() and is_array(param_step_sizes[params[0]]):
+        these_derivs = all_derivs[0:len(param_step_sizes[params[0]])]
+        other_derivs = all_derivs[len(param_step_sizes[params[0]]):]
+        if other_derivs.shape[0] == 0:
+            sets = []
+            for i in range(len(param_step_sizes[params[0]])):
+                if not np.all(these_derivs[i] == np.nan):
+                    sets.append([these_derivs[i]])
+            return sets
+        else:
+            sets = get_deriv_sets(other_derivs, params[1:], param_step_sizes)
+            output_sets = []
+            for set in sets:
+                for i in range(len(param_step_sizes[params[0]])):
+                    if not np.all(these_derivs[i] == np.nan):
+                        new_set = copy(set)
+                        new_set.insert(0, these_derivs[i])
+                        output_sets.append(new_set)
+
+            return output_sets
+
+    else:
+        if all_derivs.shape[0] == 1:
+            return [[all_derivs[0]]]
+        else:
+            sets = get_deriv_sets(all_derivs[1:], params[1:], param_step_sizes)
+            for set in sets:
+                set.insert(0, all_derivs[0])
+            return sets
