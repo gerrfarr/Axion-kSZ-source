6b4133bca55181927d54ff5ef99284a600a522ae
Gerrit Farren <gerrit.farre@gmail.com>
2021-02-23 14:37:20 +0000

    implemented full fisher procedure

--- fisher_analysis/run_axion_fisher_forecast.py
+++ fisher_analysis/run_axion_fisher_forecast.py
@@ -12,14 +12,15 @@ except Exception:
 
 from axion_kSZ_source.theory.cosmology import Cosmology,CosmologyCustomH
 from axion_kSZ_source.fisher_analysis.get_parameter_derivative import ParamDerivatives
-from axion_kSZ_source.fisher_analysis.compute_fisher_matrix import make_fisher_matrix
 from axion_kSZ_source.auxiliary.cosmo_db import CosmoDB
 from axion_kSZ_source.axion_camb_wrappers.run_axion_camb import AxionCAMBWrapper
 from axion_kSZ_source.fisher_analysis.compute_mpv import compute_mean_pairwise_velocity
+from axion_kSZ_source.fisher_analysis.compute_covariance import compute_covariance_matrix
 from axion_kSZ_source.auxiliary.survey_helper import StageII,StageIII,StageIV,SurveyType
 
 from axion_kSZ_source.auxiliary.helper_functions import is_array
 from axion_kSZ_source.auxiliary.integration_helper import IntegrationHelper
+from axion_kSZ_source.fisher_analysis.compute_fisher_matrix import make_fisher_matrix,get_deriv_sets
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
@@ -40,14 +41,14 @@ if rank==0:
 
     axion_abundance_fractional_step_sizes = np.array([0.01, 0.05, 0.1, 0.2])
 
-    parameters_numeric = ["h", "omegaCDM", "omegaB", "n_s", "A_s", "axion_frac"]
+    cosmo_params = ["h", "omegaCDM", "omegaB", "n_s", "A_s", "axion_frac"]
     parameter_fractional_step_sizes = {"h":0.05, "omegaCDM":0.05, "omegaB":0.05, "n_s":0.005, "A_s":0.005, "axion_frac":axion_abundance_fractional_step_sizes}
-    parameters_analytic = ["b"]
+    nuisance_params = ["b"]
 
     number_of_parameter_step_sizes = 0
-    for step_size in parameter_fractional_step_sizes:
+    for step_size in parameter_fractional_step_sizes.values():
         if is_array(step_size):
-            number_of_parameter_step_sizes +=len(step_size)
+            number_of_parameter_step_sizes += len(step_size)
         else:
             number_of_parameter_step_sizes += 1
 
@@ -82,6 +83,20 @@ if rank==0:
 
         return id
 
+    def cov_eval_function(cosmo):
+        ID, ran_TF, successful_TF, out_path, log_path = cosmoDB.get_by_cosmo(cosmo)
+        file_root = os.path.basename(out_path)
+        root_path = out_path[:-len(file_root)]
+        wrapper = AxionCAMBWrapper(root_path, file_root, log_path)
+
+        lin_power = wrapper.get_linear_power()
+        growth = wrapper.get_growth()
+        cosmo.set_H_interpolation(wrapper.get_hubble())
+
+        id = p_eval.add_job(compute_covariance_matrix, r_vals, delta_r, rMin, cosmo, lin_power, growth, survey, window=window, old_bias=old_bias, jenkins_mass=False, integrationHelper=intHelper, kMin=kMin, kMax=kMax)
+
+        return id
+
     def b_deriv(cosmo, wrapper, r_vals, rMin, survey, window, old_bias, intHelper, kMin, kMax):
 
         lin_power = wrapper.get_linear_power()
@@ -104,31 +119,33 @@ for i_m, m in enumerate(axion_masses):
     if rank==0:
         parameter_derivatives = []
         analytic_derivs_queue_ids = []
+        covariance_eval_ids = []
         for i_f, axion_frac in enumerate(axion_abundances):
             parameter_derivatives_tmp = {}
             analytic_derivs_queue_ids_tmp = {}
 
             fiducial_cosmo = Cosmology.generate(axion_frac=axion_frac, m_axion=m, read_H_from_file=True)
             fid_axion_camb = schedule_camb_run(fiducial_cosmo)
+            covariance_eval_ids.append(cov_eval_function(fiducial_cosmo))
 
-            for param in parameters_numeric:
-                if is_array(parameter_fractional_step_sizes[param]):
-                    parameter_derivatives_tmp[param] = []
-                    for step_size in parameter_fractional_step_sizes[param]:
-                        param_vals = getattr(fiducial_cosmo, param) * (1.0 + stencil * step_size)
-                        if param == "axion_frac" and np.max(param_vals)>=1.0 or np.min(param_vals)<=0.0:
-                            continue
+            for param in cosmo_params+nuisance_params:
+                if param in parameter_fractional_step_sizes.keys():
+                    if is_array(parameter_fractional_step_sizes[param]):
+                        parameter_derivatives_tmp[param] = []
+                        for step_size in parameter_fractional_step_sizes[param]:
+                            param_vals = getattr(fiducial_cosmo, param) * (1.0 + stencil * step_size)
+                            if param == "axion_frac" and np.max(param_vals)>=1.0 or np.min(param_vals)<=0.0:
+                                continue
+                            ds = ParamDerivatives(fiducial_cosmo, param, param_vals, mpv_eval_function, eval_function_args=(), eval_function_kwargs={}, pre_computation_function=schedule_camb_run, pre_function_args=(), pre_function_kwargs={}, stencil=stencil)
+                            ds.prep_parameters()
+                            parameter_derivatives_tmp[param].append(ds)
+                    else:
+                        param_vals = getattr(fiducial_cosmo, param)*(1.0 + stencil * parameter_fractional_step_sizes[param])
                         ds = ParamDerivatives(fiducial_cosmo, param, param_vals, mpv_eval_function, eval_function_args=(), eval_function_kwargs={}, pre_computation_function=schedule_camb_run, pre_function_args=(), pre_function_kwargs={}, stencil=stencil)
                         ds.prep_parameters()
-                        parameter_derivatives_tmp[param].append(ds)
+                        parameter_derivatives_tmp[param] = ds
                 else:
-                    param_vals = getattr(fiducial_cosmo, param)*(1.0 + stencil * parameter_fractional_step_sizes[param])
-                    ds = ParamDerivatives(fiducial_cosmo, param, param_vals, mpv_eval_function, eval_function_args=(), eval_function_kwargs={}, pre_computation_function=schedule_camb_run, pre_function_args=(), pre_function_kwargs={}, stencil=stencil)
-                    ds.prep_parameters()
-                    parameter_derivatives_tmp[param] = ds
-
-            for param in parameters_analytic:
-                analytic_derivs_queue_ids_tmp[param] = p_eval.add_job(parameters_analytic_deriv_functions[param], fiducial_cosmo, fid_axion_camb, r_vals, rMin, survey, window, old_bias, intHelper, kMin, kMax)
+                    analytic_derivs_queue_ids_tmp[param] = p_eval.add_job(parameters_analytic_deriv_functions[param], fiducial_cosmo, fid_axion_camb, r_vals, rMin, survey, window, old_bias, intHelper, kMin, kMax)
 
             parameter_derivatives.append(parameter_derivatives_tmp)
             analytic_derivs_queue_ids.append(analytic_derivs_queue_ids_tmp)
@@ -147,55 +164,40 @@ for i_m, m in enumerate(axion_masses):
 
         p_eval.run()
 
-        derivatives = np.full((len(axion_abundances), number_of_parameter_step_sizes + len(parameters_analytic), len(survey.center_z), len(r_vals)), np.nan)
+        derivatives = np.full((len(axion_abundances), number_of_parameter_step_sizes + len(nuisance_params), len(survey.center_z), len(r_vals)), np.nan)
 
         for i_f, axion_frac in enumerate(axion_abundances):
             i_param = 0
-            for param in parameters_numeric:
+            for param in cosmo_params:
                 if is_array(parameter_fractional_step_sizes[param]):
                     for i_step_size, step_size in enumerate(parameter_fractional_step_sizes[param]):
                         param_vals = getattr(fiducial_cosmo, param) * (1.0 + stencil * step_size)
                         if param == "axion_frac" and np.max(param_vals)>=1.0 or np.min(param_vals)<=0.0:
                             i_param += 1
                             continue
-                        i_param += 1
                         ds = parameter_derivatives[i_f][param][i_step_size]
                         derivatives[i_f, i_param] = ds.derivs(p_eval.outputs[ds.outputs])
+                        i_param += 1
                 else:
-                    i_param += 1
                     ds = parameter_derivatives[i_f][param]
                     derivatives[i_f, i_param, :, :] = ds.derivs(p_eval.outputs[ds.outputs])
+                    i_param += 1
 
-            for param in parameters_analytic:
-                i_param += 1
+            for param in nuisance_params:
                 derivatives[i_f, i_param, :, :] = p_eval.outputs[analytic_derivs_queue_ids[i_f][param]]
+                i_param += 1
 
         np.save(f"./test_derivs_ma={m:.3E}.dat", derivatives)
 
-        """n_combination = 1
-        for key in parameter_fractional_step_sizes.keys():
-            if is_array(parameter_fractional_step_sizes[key]):
-                n_combination*=len(parameter_fractional_step_sizes[key])
-
         for i_f, axion_frac in enumerate(axion_abundances):
-            deriv_sets = np.empty((n_combination, len(parameters_numeric)+len(parameters_analytic), len(survey.center_z), len(r_vals)))
+            covariance = p_eval.outputs[covariance_eval_ids[i_f]]
+            deriv_sets = get_deriv_sets(derivatives[i_f], cosmo_params + nuisance_params, parameter_fractional_step_sizes)
+            for d_set in deriv_sets:
+                p_fisher.add_job(make_fisher_matrix, d_set[0:-len(nuisance_params)], d_set[-len(nuisance_params):], survey.center_z, covariance)
 
-            for i_param, param in enumerate(parameters_numeric):
-                if is_array(parameter_fractional_step_sizes[param]):
-                    for i_step_size, step_size in enumerate(parameter_fractional_step_sizes[param]):
-                        param_vals = getattr(fiducial_cosmo, param) * (1.0 + stencil * step_size)
-                        if param == "axion_frac" and np.max(param_vals)>=1.0 or np.min(param_vals)<=0.0:
-                            continue
-                        #TODO
+        p_fisher.run()
 
-                else:
-                    param_vals = getattr(fiducial_cosmo, param)*(1.0 + stencil * parameter_fractional_step_sizes[param])
-                    #TODO
-
-
-            for param in parameters_analytic:
-                #TODO
-                pass"""
+        np.save(f"./test_fisher_matrices_ma={m:.3E}.dat", p_fisher.outputs)
 
 if rank==0:
     cosmoDB.save()
