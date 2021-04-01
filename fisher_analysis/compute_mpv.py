218d4d6ccdd42b4b3cbbc96cba7db9c5b948da44
Gerrit Farren <gerrit.farre@gmail.com>
2021-04-01 14:34:39 +0100

    added FFTLog and approximation options

--- fisher_analysis/compute_mpv.py
+++ fisher_analysis/compute_mpv.py
@@ -1,9 +1,11 @@
 from ..theory.cosmology import Cosmology
 from ..theory.sigma_interpolation import SigmaInterpolator
+from ..theory.sigma_interpolation_FFTLog import SigmaInterpolatorFFTLog
 from ..theory.halo_bias import HaloBias
 from ..theory.halo_bias_new import HaloBias as HaloBiasNew
 from ..theory.mass_functions import JenkinsMassFunction, PressSchechterMassFunction
 from ..theory.correlation_functions import CorrelationFunctions
+from ..theory.correlation_functions_FFTLog import CorrelationFunctions as CorrelationFunctionsFFTLog
 
 from ..axion_camb_wrappers.growth_interpolation import GrowthInterpolation
 from ..axion_camb_wrappers.power_interpolation import LinearPowerInterpolation
@@ -11,8 +13,10 @@ from ..axion_camb_wrappers.power_interpolation import LinearPowerInterpolation
 from ..auxiliary.integration_helper import IntegrationHelper
 from ..auxiliary.survey_helper import SurveyType
 
+import numpy as np
 
-def compute_mean_pairwise_velocity(r_vals, rMin, cosmo, lin_power, growth, survey, window="gaussian", old_bias=False, jenkins_mass=False, integrationHelper=None, kMin=1.0e-4, kMax=1.0e2, do_unbiased=False, get_correlation_functions=False):
+
+def compute_mean_pairwise_velocity(r_vals, rMin, cosmo, lin_power, growth, survey, window="gaussian", old_bias=False, jenkins_mass=False, integrationHelper=None, kMin=1.0e-4, kMax=1.0e2, do_unbiased=False, get_correlation_functions=False, use_approximations=False, use_FFTLog=False):
     """
 
     Parameters
@@ -27,8 +31,12 @@ def compute_mean_pairwise_velocity(r_vals, rMin, cosmo, lin_power, growth, surve
     if integrationHelper is None:
         integrationHelper = IntegrationHelper(1024)
 
-    sigmaInt = SigmaInterpolator(cosmo, lin_power, growth, survey.mMin, survey.mMax, survey.center_z, integrationHelper, Nr=1024, window_function=window)
-    sigmaInt.compute(kMin, kMax, do_dr=True, do_dloga=False)
+    if use_FFTLog:
+        sigmaInt = SigmaInterpolatorFFTLog(cosmo, lin_power, growth, survey.center_z, kMin, kMax, Nr=1024, window_function=window)
+        sigmaInt.compute(do_dr=True, do_dloga=False)
+    else:
+        sigmaInt = SigmaInterpolator(cosmo, lin_power, growth, survey.mMin, survey.mMax, survey.center_z, integrationHelper, Nr=1024, window_function=window)
+        sigmaInt.compute(kMin, kMax, do_dr=True, do_dloga=False)
 
     if jenkins_mass:
         mass_function = JenkinsMassFunction(cosmo, sigmaInt)
@@ -40,12 +48,21 @@ def compute_mean_pairwise_velocity(r_vals, rMin, cosmo, lin_power, growth, surve
     else:
         halo_bias = HaloBiasNew(cosmo, sigmaInt, mass_function, survey.mMin, survey.mMax, kMin, kMax, survey.center_z, integrationHelper, Nk=1024, window_function=window)
 
-    halo_bias.compute()
+    if not use_approximations:
+        halo_bias.compute()
+    else:
+        halo_bias.compute_approximation()
 
-    corr = CorrelationFunctions(cosmo, lin_power, growth, halo_bias, kMin, halo_bias._kMax if window=="sharp_k" and old_bias and kMax>halo_bias._kMax else kMax, survey.center_z, rMin, r_vals, integrationHelper)
 
+    if use_FFTLog:
+        corr = CorrelationFunctions(cosmo, lin_power, growth, halo_bias, kMin, halo_bias._kMax if window == "sharp_k" and old_bias and kMax > halo_bias._kMax else kMax, survey.center_z, rMin, integrationHelper)
+    else:
+        corr = CorrelationFunctionsFFTLog(cosmo, lin_power, growth, halo_bias, kMin, halo_bias._kMax if window == "sharp_k" and old_bias and kMax > halo_bias._kMax else kMax, survey.center_z, rMin, r_vals, integrationHelper)
+
+    rMesh,zMesh = np.meshgrid(r_vals, survey.center_z)
     if do_unbiased:
-        xi_unbiased, xi, dbarxi_dloga_unbiased, dbarxi_dloga =  corr.compute(unbiased=True, old_bias=old_bias)
+        corr.compute(unbiased=True, old_bias=old_bias)
+        xi_unbiased, xi, dbarxi_dloga_unbiased, dbarxi_dloga =  corr.get_correlation_functions(rMesh, zMesh, unbiased=True)
         v = r_vals * 100 * dbarxi_dloga / (3 * (1 + xi))
         v_dm = r_vals * 100 * dbarxi_dloga_unbiased / (3 * (1 + xi_unbiased))
         if get_correlation_functions:
@@ -53,7 +70,8 @@ def compute_mean_pairwise_velocity(r_vals, rMin, cosmo, lin_power, growth, surve
         else:
             return v, v_dm
     else:
-        xi, dbarxi_dloga = corr.compute(unbiased=False, old_bias=old_bias)
+        corr.compute(unbiased=False, old_bias=old_bias)
+        xi, dbarxi_dloga = corr.get_correlation_functions(rMesh, zMesh, unbiased=False)
         v = r_vals * 100 * dbarxi_dloga / (3 * (1 + xi))
         if get_correlation_functions:
             return v, xi, dbarxi_dloga
