ea91beed28be364a26a5bdc6ebe7b48c4ae486d5
Gerrit Farren <gerrit.farre@gmail.com>
2021-04-06 11:25:51 +0100

    Revert "minor fix"
    This reverts commit 22b10275

--- auxiliary/configs.py
+++ auxiliary/configs.py
@@ -1,5 +1,5 @@
-param_file_path="/Users/gerrit/SynologyDrive/College/Research/kSZ/axionCAMB/inifiles/params.ini "
-axion_camb_path="/Users/gerrit/SynologyDrive/College/Research/kSZ/axionCAMB/camb"
+param_file_path="/home/r/rbond/gfarren/axion kSZ/axionCAMB/inifiles/params.ini "
+axion_camb_path="/home/r/rbond/gfarren/axion kSZ/axionCAMB/camb"
 
 cosmo_database="/scratch/r/rbond/gfarren/axion_kSZ/CAMB_outputs/cosmo_db_FFTLog_test1.dat"
 
