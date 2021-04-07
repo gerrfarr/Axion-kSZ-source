16ecc8f4e818fcdcdd657adb1303090fdb7f3c18
Gerrit Farren <gerrit.farre@gmail.com>
2021-04-07 12:40:57 +0100

    computing normalised D from dlnD/dlna (better behaved numerically)

--- axion_camb_wrappers/growth_interpolation.py
+++ axion_camb_wrappers/growth_interpolation.py
@@ -5,13 +5,15 @@ class GrowthInterpolation(object):
 
     def __init__(self, file_root, degree=3, smoothing=0.0, w=1e-4):
         self.__f_data = np.loadtxt(file_root + '_devolution.dat')
-        self.__delta_data = np.loadtxt(file_root + '_evolution.dat')
+
         a_data = np.loadtxt(file_root + '_a_vals.dat')
         self.__a_vals = a_data[:, 0]
         assert (a_data[-1, 0] == 1.0)
         self.__k_vals = np.loadtxt(file_root + '_matterpower_out.dat')[:, 0]
 
-        """normalized_dataset = self.__delta_data / np.meshgrid(self.__a_vals, self.__delta_data[:, -1])[1]
+        """
+        self.__delta_data = np.loadtxt(file_root + '_evolution.dat')
+        normalized_dataset = self.__delta_data / np.meshgrid(self.__a_vals, self.__delta_data[:, -1])[1]
 
         self.__G_interpolation = RectBivariateSpline(np.log(self.__k_vals), np.log(self.__a_vals), normalized_dataset, bbox=[min(np.log(self.__k_vals)), max(np.log(self.__k_vals)), min(np.log(self.__a_vals)), max(np.log(self.__a_vals))], kx=degree, ky=degree, s=smoothing)
         self.__G_func = lambda k, a: np.squeeze(self.__G_interpolation.ev(np.log(k), np.log(a)))"""
