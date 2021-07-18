36ed7bca91943fe01a9390daff3261eda900965b
Gerrit Farren <gerrit.farre@gmail.com>
2021-07-18 17:58:29 +0100

    corrected typo

--- ostriker_vishniac/compute_ov_spectra.py
+++ ostriker_vishniac/compute_ov_spectra.py
@@ -16,7 +16,7 @@ from scipy.interpolate import interp1d as interpolate
 from scipy.integrate import cumtrapz
 
 
-def(cosmo, axionCAMBWrapper, integrationHelper=None):
+def compute_ov_spectra(cosmo, axionCAMBWrapper, integrationHelper=None):
     """
 
     Parameters
