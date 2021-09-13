c19b1d3c1c049cb8e903c7aee184a10ef5a4f6a0
Gerrit Farren <gerrit.farre@gmail.com>
2021-09-13 15:05:00 +0100

    new deterministic identifier

--- auxiliary/helper_functions.py
+++ auxiliary/helper_functions.py
@@ -1,4 +1,7 @@
 from ..theory.cosmology import Cosmology, CosmologyCustomH
+import hashlib
+import dataclasses
+import json
 
 def is_array(item, cl=None):
     try:
@@ -11,9 +14,26 @@ def is_array(item, cl=None):
     except AttributeError as ex:
         return False
 
+def json_default(thing):
+    try:
+        return dataclasses.asdict(thing)
+    except TypeError:
+        pass
+    raise TypeError(f"object of type {type(thing).__name__} not serializable")
+
+def json_dumps(thing):
+    return json.dumps(
+        thing,
+        default=json_default,
+        ensure_ascii=False,
+        sort_keys=True,
+        indent=None,
+        separators=(',', ':'),
+    )
+
 def generate_cosmo_identifier(cosmo):
     """
 
     :type cosmo: Cosmology
     """
-    return hash((cosmo.h, cosmo.omegaCDM, cosmo.omegaB, cosmo.omega_axion, cosmo.m_axion, cosmo.n_s, cosmo.A_s, type(cosmo) == CosmologyCustomH))
+    return hashlib.md5(json_dumps((cosmo.h, cosmo.omegaCDM, cosmo.omegaB, cosmo.omega_axion, cosmo.m_axion, cosmo.n_s, cosmo.A_s, type(cosmo) == CosmologyCustomH)).encode('utf-8')).digest()
